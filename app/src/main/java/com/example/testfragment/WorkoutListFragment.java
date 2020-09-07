d63bd9662bf2fbb2fadac545e35e2173ca99618a
Максим Толстобров <maximah1989@yandex.ru>
2020-09-07 11:17:08 +0300

    Create ListFragment

---
+++ app/src/main/java/com/example/testfragment/WorkoutListFragment.java
@@ -0,0 +1,32 @@
+package com.example.testfragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class WorkoutListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] names = new String[Workout.workouts.length];
+        for(int i = 0; i < names.length; i++){
+            names[i] = Workout.workouts[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(), android.R.layout.simple_list_item_1, names
+        );
+        setListAdapter(adapter);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
